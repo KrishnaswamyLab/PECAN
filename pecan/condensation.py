fd257c2a622cf7a39ee83d0bbec4aa89a1c0d061
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2020-11-13 15:51:37 +0100

    Finished first implementation of diffusion condensation

--- pecan/condensation.py
+++ pecan/condensation.py
@@ -19,17 +19,40 @@ def condensation(X, epsilon):
     n = X.shape[0]
 
     # Denotes the previous density measurement, which is initialised to
-    # an identity matrix depending on the number of samples.
+    # an identity matrix depending on the number of samples, as well as
+    # the previously-observed difference, which has to be set to inf in
+    # order to be suitable.
     Q_prev = np.identity(n)
+    Q_diff = np.inf
 
+    # We only work on a copy of the data set since we shift everything,
+    # i.e. points start changing their positions.
     X = X.copy()
-    A = make_affinity_matrix(X, epsilon)
-    Q = np.sum(A, axis=1)
-    K = np.diag(1.0 / Q) @ A @ np.diag(1.0 / Q)
-    P = np.diag(1.0 / np.sum(K, axis=1)) @ K
-    X = P @ X
 
-    Q_diff = np.max(Q - Q_prev)
+    i = 0
+    j = -2
+
+    while i - j > 1:
+
+        j = i
+
+        while Q_diff >= 1e-4:
+
+            # This signals that we want to perform an additional
+            # operation of diffusion here.
+            i += 1
+
+            A = make_affinity_matrix(X, epsilon)
+            Q = np.sum(A, axis=1)
+            K = np.diag(1.0 / Q) @ A @ np.diag(1.0 / Q)
+            P = np.diag(1.0 / np.sum(K, axis=1)) @ K
+            X = P @ X
+
+            Q_diff = np.max(Q - Q_prev)
+            Q_prev = Q
+
+        epsilon *= 2
+        Q_diff = np.inf
 
 
 if __name__ == '__main__':
