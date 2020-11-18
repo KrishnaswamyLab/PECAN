56ba51c03ba37299b7f19ca06c0c1ed5fda105b1
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2020-11-18 17:42:56 +0100

    Added ellipse and circle generators

--- pecan/data.py
+++ pecan/data.py
@@ -26,3 +26,50 @@ def make_barbell(N, beta=1):
             C.append(2)
 
     return np.asarray(X), np.asarray(C)
+
+
+def hyperuniform_circle(N):
+    """Generate hyperuniformly-Sampled 2-D circle and colours."""
+    X = []
+    C = np.linspace(0, 1, N)
+
+    theta = np.linspace(0, 2*np.pi, N, endpoint=False)
+
+    for t in theta:
+        X.append((np.cos(t), np.sin(t)))
+
+    return np.asarray(X), np.asarray(C)
+
+
+def hyperuniform_ellipse(N, a=1, b=2):
+    """Generate hyperuniformly-sampled 2-D ellipse with colours."""
+    assert(a < b) # a must be length of minor semi-axis; b major semi-axis
+
+    X = []
+    C = np.linspace(0, 1, N)
+
+    angles = 2 * np.pi * np.arange(N) / N
+
+    if a != b:
+        # Given N points, combine scipy elliptic integral + optimize to
+        # find N equidistant points along ellipse manifold, then
+        # convert to angles.
+
+        from scipy import special
+        from scipy import optimize
+
+        e = np.sqrt(1.0 - a**2 / b**2)
+        tot_size = special.ellipeinc(2.0*np.pi, e)
+        arc_size = tot_size/N
+        arcs = np.arange(N) * arc_size
+        res = optimize.root(
+                lambda x: (special.ellipeinc(x, e) - arcs), angles
+        )
+        angles = res.x
+
+        arcs = special.ellipeinc(angles, e)
+
+    for t in angles:
+        X.append((a*np.cos(t), b*np.sin(t)))
+
+    return np.asarray(X), np.asarray(C)
