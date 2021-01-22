2a2151c350021bc4e47fc4f1886537ee22a1eeeb
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2021-01-22 17:44:19 +0100

    Disabling normalisation for infinity norm

--- pecan/analyse_persistence.py
+++ pecan/analyse_persistence.py
@@ -39,8 +39,11 @@ def process_file(filename):
         for diagram in persistence_diagrams
     ])
 
-    # Ensures comparability of the values.
-    values = values / np.max(values)
+    # Ensures comparability of the values, but only for total
+    # persistence calculations.
+    if summary_fn == total_persistence:
+        values = values / np.max(values)
+
     return values
 
 
