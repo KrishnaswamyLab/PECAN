b7d8a65235ea92542d4c60106119e5cfb5d0fdd8
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2021-07-20 10:29:00 +0200

    Added simple box kernel

--- pecan/kernels.py
+++ pecan/kernels.py
@@ -1,8 +1,11 @@
 """Kernel functions and related methods."""
 
+import warnings
+
 import numpy as np
 
 from sklearn.metrics.pairwise import laplacian_kernel
+from sklearn.metrics.pairwise import euclidean_distances
 
 
 def get_kernel_fn(kernel):
@@ -21,7 +24,23 @@ def get_kernel_fn(kernel):
             K = np.full((n, n), 1.0 / epsilon)
             return K
         return kernel_fn
+    elif kernel == 'box':
+        def kernel_fn(X, epsilon):
+            K = euclidean_distances(X)
+
+            # Use mask because we have to ensure that the same positions
+            # are being matched when setting kernel values manually.
+            mask = K <= epsilon
+
+            K[mask] = 1.0
+            K[~mask] = 0.0
+
+            return K
+        return kernel_fn
+
+    warnings.warn(
+        f'Falling back to default kernel instead of using kernel "{kernel}".'
+    )
 
     # Fall back to default kernel.
     return None
-
