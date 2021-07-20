a8b4f233b21c455eee1a91b70fee95504da597cf
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2021-07-20 11:29:28 +0200

    Improved publication script

--- pecan/scripts/publication.sh
+++ pecan/scripts/publication.sh
@@ -4,6 +4,8 @@
 # free to add additional scenarios here, but check that each output name
 # is unique---else, the script will overwrite everything.
 
+SEED=2021
+
 N_POINTS=128
 DATASETS=(blobs hyperuniform_circle petals simplex)
 
@@ -12,8 +14,9 @@ KERNELS=(box gaussian laplacian)
 for DATASET in "${DATASETS[@]}"; do
   for KERNEL in "${KERNELS[@]}"; do
     echo "Running condensation for $DATASET with '$KERNEL' kernel..."
-    poetry run python condensation.py --kernel $KERNEL              \
-                                      --data $DATASET               \
+    poetry run python condensation.py --kernel ${KERNEL}            \
+                                      --data ${DATASET}             \
+                                      -s ${SEED}                    \
                                       -n ${N_POINTS}                \
                                       -c CalculateDiffusionHomology \
                                       -o data/publication/${DATASET}_${KERNEL}_n${N_POINTS}.npz 
