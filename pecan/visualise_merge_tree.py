c0e23cfdadef82721df9eaf2f27d64de69df5976
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2021-08-17 20:27:00 +0200

    Marginal improvements of merge tree visualisation

--- pecan/visualise_merge_tree.py
+++ pecan/visualise_merge_tree.py
@@ -8,18 +8,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 import networkx as nx
 
-from scipy.cluster.hierarchy import dendrogram
-
+from utilities import make_tensor
 from utilities import parse_keys
+from utilities import UnionFind
 
 
-def make_tree(edges):
+def make_tree(edges, n_vertices):
     """Create tree from edges."""
-    G = nx.DiGraph()
+    G = nx.Graph()
+    G.add_nodes_from(range(n_vertices))
+
+    uf = UnionFind(n_vertices)
 
     for t, u, v in edges:
         G.add_edge(u, v, time=t)
 
+        uf.merge(u, v)
+
+    # TODO: do something with `uf.roots()`?
     return G
 
 
@@ -48,12 +54,18 @@ if __name__ == '__main__':
     data = np.load(args.INPUT)
     parsed_keys = parse_keys(data)
 
+    n_vertices = make_tensor(data, parsed_keys['data']).shape[0]
+
     assert 'diffusion_homology_edges' in parsed_keys, \
         'Require "diffusion_homology_edges" key'
 
     edges = data['diffusion_homology_edges']
-    tree = make_tree(edges)
-    pos = nx.nx_pydot.graphviz_layout(tree, prog='dot')
+    tree = make_tree(edges, n_vertices)
+    pos = nx.nx_agraph.graphviz_layout(
+        tree,
+        prog='dot',
+        root=0,
+    )
 
     nx.draw(tree, pos=pos, with_labels=True)
 
