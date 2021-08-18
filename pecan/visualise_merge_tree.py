60c219676cf058352c805a205795e77ec9df6f65
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2021-08-18 10:08:11 +0200

    Improved calculation of cluster size

--- pecan/visualise_merge_tree.py
+++ pecan/visualise_merge_tree.py
@@ -6,36 +6,18 @@ import sys
 import matplotlib.pyplot as plt
 
 import numpy as np
-import networkx as nx
 
 from scipy.cluster.hierarchy import dendrogram
 
 from utilities import make_tensor
 from utilities import parse_keys
-from utilities import UnionFind
-
-
-def make_tree(edges, n_vertices):
-    """Create tree from edges."""
-    G = nx.Graph()
-    G.add_nodes_from(range(n_vertices))
-
-    uf = UnionFind(n_vertices)
-
-    for t, u, v in edges:
-        G.add_edge(u, v, time=t)
-
-        uf.merge(u, v)
-
-    # TODO: do something with `uf.roots()`?
-    return G
 
 
 def make_linkage_matrix(edges, n_vertices):
     """Create linkage matrix from edges."""
-    # Store representatives of each point. This is required because the
-    # linkage matrix does not permit us to reuse the indices of the
-    # original observations more than once.
+    # Store representatives and cluster sizes of each point. This is
+    # required because the linkage matrix does not permit us to reuse
+    # the indices of the original observations more than once.
     reps = {}
 
     # Prepare linkage matrix; this follows the number of merge steps of
@@ -43,20 +25,20 @@ def make_linkage_matrix(edges, n_vertices):
     Z = np.zeros((len(edges), 4))
 
     for i, (t, u, v) in enumerate(edges):
-        rep_u = reps.get(u, u)
-        rep_v = reps.get(v, v)
+        rep_u = reps.get(u, (u, 1))
+        rep_v = reps.get(v, (v, 1))
 
-        Z[i, 0] = rep_u
-        Z[i, 1] = rep_v
+        Z[i, 0] = rep_u[0]
+        Z[i, 1] = rep_v[0]
         Z[i, 2] = t
 
-        # TODO: need number of observations here
-        Z[i, 3] = 2
+        n_observations = rep_u[1] + rep_v[1]
+        Z[i, 3] = n_observations
 
         # The representation for both of the nodes now has to be
         # updated.
-        reps[u] = i + n_vertices
-        reps[v] = i + n_vertices
+        reps[u] = (i + n_vertices, n_observations)
+        reps[v] = (i + n_vertices, n_observations)
 
     return Z
 
@@ -92,16 +74,8 @@ if __name__ == '__main__':
         'Require "diffusion_homology_edges" key'
 
     edges = data['diffusion_homology_edges']
-    tree = make_tree(edges, n_vertices)
-    pos = nx.nx_agraph.graphviz_layout(
-        tree,
-        prog='dot',
-        root=0,
-    )
-
-    nx.draw(tree, pos=pos, with_labels=True)
-
     Z = make_linkage_matrix(edges, n_vertices)
+
     dendrogram(Z)
 
     plt.tight_layout()
