3c436260b04373c9482a2b36d06f84e1802dc15d
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2021-08-18 08:45:33 +0200

    Proper linkage matrix conversion

--- pecan/visualise_merge_tree.py
+++ pecan/visualise_merge_tree.py
@@ -8,6 +8,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import networkx as nx
 
+from scipy.cluster.hierarchy import dendrogram
+
 from utilities import make_tensor
 from utilities import parse_keys
 from utilities import UnionFind
@@ -27,6 +29,36 @@ def make_tree(edges, n_vertices):
 
     # TODO: do something with `uf.roots()`?
     return G
+
+
+def make_linkage_matrix(edges, n_vertices):
+    """Create linkage matrix from edges."""
+    # Store representatives of each point. This is required because the
+    # linkage matrix does not permit us to reuse the indices of the
+    # original observations more than once.
+    reps = {}
+
+    # Prepare linkage matrix; this follows the number of merge steps of
+    # the algorithm.
+    Z = np.zeros((len(edges), 4))
+
+    for i, (t, u, v) in enumerate(edges):
+        rep_u = reps.get(u, u)
+        rep_v = reps.get(v, v)
+
+        Z[i, 0] = rep_u
+        Z[i, 1] = rep_v
+        Z[i, 2] = t
+
+        # TODO: need number of observations here
+        Z[i, 3] = 2
+
+        # The representation for both of the nodes now has to be
+        # updated.
+        reps[u] = i + n_vertices
+        reps[v] = i + n_vertices
+
+    return Z
 
 
 if __name__ == '__main__':
@@ -68,6 +100,9 @@ if __name__ == '__main__':
     )
 
     nx.draw(tree, pos=pos, with_labels=True)
+
+    Z = make_linkage_matrix(edges, n_vertices)
+    dendrogram(Z)
 
     plt.tight_layout()
     plt.show()
