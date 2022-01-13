314176af6cb485483a4eb09e784b43138de449a7
Bastian Rieck <bastian@rieck.me>
2022-01-13 11:23:08 +0100

    Calculating diameter

--- pecan/convex_hull.py
+++ pecan/convex_hull.py
@@ -9,10 +9,13 @@ the point cloud.
 import argparse
 import sys
 
+import numpy as np
 import pandas as pd
 
 from scipy.spatial import ConvexHull
 from scipy.spatial.qhull import QhullError
+
+from sklearn.metrics import pairwise_distances
 
 
 if __name__ == '__main__':
@@ -32,13 +35,23 @@ if __name__ == '__main__':
 
         vertices = hull.vertices
 
-        print(filename, file=sys.stderr)
+        print('\n', filename, file=sys.stderr)
 
         # Generate lines for the convex hull (in TikZ format)
-
         output = '\\draw '
         for x, y in zip(vertices, vertices[1:]):
             output += f'({x:.02f},{y:.02f}) -- '
 
         output += 'cycle;'
         print(output)
+
+        # Find diameter
+        dist = pairwise_distances(X)
+        index = np.unravel_index(np.argmax(dist), dist.shape)
+
+        a, b = X[index, :]
+
+        diam_line = '\\draw '
+        diam_line += f'({a[0]:.02f},{a[1]:.02f}) -- '
+        diam_line += f'({b[0]:.02f},{b[1]:.02f});'
+        print(diam_line)
