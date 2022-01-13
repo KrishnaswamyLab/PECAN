1f90fe2fe1fa81a0033620aa3940e3b38ce0319a
Bastian Rieck <bastian@rieck.me>
2022-01-13 11:41:09 +0100

    Calculating extents

--- pecan/convex_hull.py
+++ pecan/convex_hull.py
@@ -17,12 +17,16 @@ from scipy.spatial.qhull import QhullError
 
 from sklearn.metrics import pairwise_distances
 
+from utilities import get_limits
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('INPUT', help='Input file(s)', nargs='+')
 
     args = parser.parse_args()
+
+    x_min, x_max, y_min, y_max = np.inf, -np.inf, np.inf, -np.inf
 
     for filename in args.INPUT:
         X = pd.read_csv(filename, sep='\t').to_numpy()
@@ -35,7 +39,7 @@ if __name__ == '__main__':
 
         vertices = hull.vertices
 
-        print('\n', filename, file=sys.stderr)
+        print(f'\n{filename}', file=sys.stderr)
 
         # Generate lines for the convex hull (in TikZ format)
         output = '\\draw '
@@ -56,3 +60,14 @@ if __name__ == '__main__':
         diam_line += f'({a[0]:.02f},{a[1]:.02f}) -- '
         diam_line += f'({b[0]:.02f},{b[1]:.02f});'
         print(diam_line)
+
+        x_min_, x_max_, y_min_, y_max_ = get_limits(X)
+
+        x_min = min(x_min, x_min_)
+        x_max = max(x_max, x_max_)
+        y_min = min(y_min, y_min_)
+        y_max = max(y_max, y_max_)
+
+    print('\nExtents:')
+    print(f'{x_min:.2f}, {y_min:.2f}')
+    print(f'{x_max:.2f}, {y_max:.2f}')
