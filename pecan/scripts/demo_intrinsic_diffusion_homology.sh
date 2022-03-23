3fcdafe54842ec3f190e4d9e6ee5f5f55fafa7da
Bastian Rieck <bastian@rieck.me>
2022-03-23 11:26:20 +0100

    Wrote demo script for intrinsic diffusion homology testing

---
+++ pecan/scripts/demo_intrinsic_diffusion_homology.sh
@@ -0,0 +1,13 @@
+DATASET=$1
+N_POINTS=$2
+KERNEL=gaussian
+SEED=23
+
+poetry run python condensation.py --kernel ${KERNEL}            \
+                                  --data ${DATASET}             \
+                                  -s ${SEED}                    \
+                                  -n ${N_POINTS}                \
+                                  -c CalculateDiffusionHomology \
+                                  -o /tmp/test.npz
+
+poetry run python calculate_intrinsic_diffusion_homology.py /tmp/test.npz
