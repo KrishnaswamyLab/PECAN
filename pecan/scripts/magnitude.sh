3dc0042dcd17426e54948a21a849f3a2ee264482
Bastian Rieck <bastian@rieck.me>
2024-05-10 19:17:28 +0200

    Changed data set for magnitude test

--- pecan/scripts/magnitude.sh
+++ pecan/scripts/magnitude.sh
@@ -3,6 +3,6 @@ echo > /tmp/mag.txt
 
 for i in `seq 20`; do
   echo $i
-  python condensation.py -n $(expr $i + 128) -c CalculateMagnitude -d petals -o test.npz --force
+  python condensation.py -n 128 -c CalculateMagnitude -d annulus -o test.npz --force
   python extract_data.py test.npz >> /tmp/mag.txt
 done
